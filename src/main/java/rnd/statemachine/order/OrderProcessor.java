17e0050d70129de5cfc187c2161a27eb9c3544e6
Nalla Senthilnathan <nrsenthil@gmail.com>
2019-12-22 15:17:02 -0500

    Lombok revisions

--- src/main/java/rnd/statemachine/order/OrderProcessor.java
+++ src/main/java/rnd/statemachine/order/OrderProcessor.java
@@ -4,17 +4,15 @@ import java.util.function.Consumer;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import rnd.statemachine.ProcessData;
 import rnd.statemachine.Processor;
 
 @Service
+@RequiredArgsConstructor
 public class OrderProcessor implements Processor {
 	
 	private final OrderProcessorHelper helper;
-	
-	public OrderProcessor(OrderProcessorHelper helper) {
-		this.helper = helper;
-	}
         
     @Override
     public void process(ProcessData data, Consumer<ProcessData> consumerFn) {   
--- src/main/java/rnd/statemachine/order/PaymentProcessor.java
+++ src/main/java/rnd/statemachine/order/PaymentProcessor.java
@@ -4,17 +4,15 @@ import java.util.function.Consumer;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
 import rnd.statemachine.ProcessData;
 import rnd.statemachine.Processor;
 
 @Service
+@RequiredArgsConstructor
 public class PaymentProcessor implements Processor {
 	
 	private final PaymentProcessorHelper helper;
-	
-	public PaymentProcessor(PaymentProcessorHelper helper) {
-		this.helper = helper;
-	}
     
     @Override
     public void process(ProcessData data, Consumer<ProcessData> consumerFn) {
