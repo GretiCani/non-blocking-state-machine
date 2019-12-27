dcc334e3284ae5558b62003c2c854a0f743e63eb
Nalla Senthilnathan <nrsenthil@gmail.com>
2019-12-27 18:40:37 -0500

    cleanup

--- src/main/java/rnd/statemachine/ProcessEvent.java
+++ src/main/java/rnd/statemachine/ProcessEvent.java
@@ -1,5 +1,8 @@
 package rnd.statemachine;
-
+/**
+ * The Enum which is configured with state transitions
+ * should implement this interface
+ */
 public interface ProcessEvent {
     public abstract Class<? extends AbstractProcessor> nextStepProcessor();   
     public abstract ProcessState nextState();
--- src/main/java/rnd/statemachine/StateMachineApplication.java
+++ src/main/java/rnd/statemachine/StateMachineApplication.java
@@ -6,15 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * A simple state machine implementation for Spring Boot projects Includes a
  * framework and a sample application of the framework to an online order
- * process
+ * process. This version enables one or more processors to be non-blocking.
  * 
- * @author Nalla Senthilnathan https://github.com/mapteb/simple-state-machine
+ * @author Nalla Senthilnathan https://github.com/mapteb/non-blocking-state-machine
  *
  */
-@SpringBootApplication(scanBasePackages= {"rnd.statemachine", "rnd.statemachine.order"})
+@SpringBootApplication
 public class StateMachineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StateMachineApplication.class, args);
 	}
-
 }
